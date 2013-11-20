dee63d53546520300a0f78f96a8f5a74536f6f80
rwalpole <robkwalpole@gmail.com>
2013-11-20 17:26:34 +0000

    [bugfix] first attempt at fixing test

--- src/test/java/com/epimorphics/lda/tests/TestConstruct.java
+++ src/test/java/com/epimorphics/lda/tests/TestConstruct.java
@@ -36,7 +36,7 @@ public class TestConstruct {
 				"",
 				"",
 				"; ext:construct '" +
-						"SELECT{?parent dri:childCount ?childCount}"+
+						"CONSTRUCT{?parent dri:childCount ?childCount}"+
 						"WHERE{SELECT ?parent (COUNT(?child) AS ?childCount)"+
 						"WHERE {?parent dri:catalogUUID ?uuid ."+
 						"?child dri:catalogParentUUID ?parent."+
@@ -55,64 +55,57 @@ public class TestConstruct {
 	*/
 	public void testConstruct( String params, String inSpec, String inEndpoint ) {
 		
-		String prefixes = "@prefix : <eh:/> . @prefix dri: <http://nationalarchives.gov.uk/terms/dri#> .";
-		
-		Model specModel = modelFrom(
-				prefixes
-				, "@prefix api:     <http://purl.org/linked-data/api/vocab#> ."
-				, "@prefix dc:      <http://purl.org/dc/elements/1.1/> ."
-				, "@prefix geo:     <http://www.w3.org/2003/01/geo/wgs84_pos#> ."
-				, "@prefix owl:     <http://www.w3.org/2002/07/owl#> ."
-				, "@prefix rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ."
-				, "@prefix xsd:     <http://www.w3.org/2001/XMLSchema#> ."
-				, "@prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#> ."
-				, "@prefix foaf:    <http://xmlns.com/foaf/0.1/> ."
-				, "@prefix school:  <http://education.data.gov.uk/def/school/> ."
-				, "@prefix skos:    <http://www.w3.org/2004/02/skos/core#> ."
-				, "@prefix ext:     <http://wwww.devexe.co.uk/vocabularies/ext#> ."
-				, "@prefix dri:     <http://nationalarchives.gov.uk/terms/dri#> ."
-			//
-				, ":root a api:API"
-				, "; api:sparqlEndpoint <here:dataPart>"
-				, inSpec
-				, "; api:endpoint :ep"
-				, "."
-			//
-				, ":ep a api:ItemEndpoint"
-				, "; api:variable [api:name \"uuid\"; api:type xsd:string]"
-				, "; api:uriTemplate '/child-count/{uuid}'"
-				, inEndpoint
-				, "."
-			//
-				, "<here:dataPart> :elements <http://localhost:2020/dri/catalog/45b57b49-9abf-4fe0-b82d-87c984564a6c>,"
-				, "<http://localhost:2020/dri/catalog/e2ddcb69-55b0-4816-9792-d0abab5dc6f9>,"
-				, "<http://localhost:2020/dri/catalog/6a1eb17a-7c08-4927-a71a-0cd6c29c252a>"
-				, "."
-			//
-				, "<http://localhost:2020/dri/catalog/45b57b49-9abf-4fe0-b82d-87c984564a6c> a dri:catalog ;"
-				, "dri:catalogBatchUUID <http://localhost:2020/dri/batch/08dacdd9-9f81-4411-a87b-0b68185ecfa7> ;"
-				, "dri:catalogCollectionUUID <http://localhost:2020/dri/collection/e9f3c8e9-e883-4fcf-a9a3-5caf0c808c5d> ;"
-				, "dri:catalogKnown  \"2012-06-04T00:00:00\"^^xsd:dateTime ;"
-				, "dri:catalogReference \"WO/16/409/27_31\"^^xsd:string ;"
-				, "dri:catalogUUID  \"45b57b49-9abf-4fe0-b82d-87c984564a6c\"^^xsd:string ."
-			//
-				, "<http://localhost:2020/dri/catalog/e2ddcb69-55b0-4816-9792-d0abab5dc6f9> a dri:catalog ;"
-				, "dri:catalogBatchUUID <http://localhost:2020/dri/batch/08dacdd9-9f81-4411-a87b-0b68185ecfa7> ;"
-				, "dri:catalogCollectionUUID <http://localhost:2020/dri/collection/e9f3c8e9-e883-4fcf-a9a3-5caf0c808c5d> ;"
-				, "dri:catalogKnown  \"2012-06-04T00:00:00\"^^xsd:dateTime ;"
-				, "dri:catalogParentUUID <http://localhost:2020/dri/catalog/45b57b49-9abf-4fe0-b82d-87c984564a6c> ;"
-				, "dri:catalogReference \"WO/16/409/27_31/112\"^^xsd:string ;"
-				, "dri:catalogUUID  \"e2ddcb69-55b0-4816-9792-d0abab5dc6f9\"^^xsd:string ."
-			//
-				, "<http://localhost:2020/dri/catalog/6a1eb17a-7c08-4927-a71a-0cd6c29c252a> a dri:catalog ;"
-				, "dri:catalogBatchUUID <http://localhost:2020/dri/batch/08dacdd9-9f81-4411-a87b-0b68185ecfa7> ;"
-				, "dri:catalogCollectionUUID <http://localhost:2020/dri/collection/e9f3c8e9-e883-4fcf-a9a3-5caf0c808c5d> ;"
-				, "dri:catalogKnown  \"2012-06-04T00:00:00\"^^xsd:dateTime ;"
-				, "dri:catalogParentUUID <http://localhost:2020/dri/catalog/45b57b49-9abf-4fe0-b82d-87c984564a6c> ;"
-				, "dri:catalogReference \"WO/16/409/27_31/142\"^^xsd:string ;"
-				, "dri:catalogUUID  \"6a1eb17a-7c08-4927-a71a-0cd6c29c252a\"^^xsd:string ."
-				);
-	
+		String prefixes = "@prefix : <eh:/> . @prefix dri: <http://wwww.devexe.co.uk/vocabularies/dri#> .";
+
+        StringBuilder builder = new StringBuilder(prefixes);
+        builder.append("@prefix api:     <http://purl.org/linked-data/api/vocab#> .");
+        builder.append("@prefix dc:      <http://purl.org/dc/elements/1.1/> .");
+        builder.append("@prefix geo:     <http://www.w3.org/2003/01/geo/wgs84_pos#> .");
+        builder.append("@prefix owl:     <http://www.w3.org/2002/07/owl#> .");
+        builder.append("@prefix rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .");
+        builder.append("@prefix xsd:     <http://www.w3.org/2001/XMLSchema#> .");
+        builder.append("@prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#> .");
+        builder.append("@prefix foaf:    <http://xmlns.com/foaf/0.1/> .");
+        builder.append("@prefix school:  <http://education.data.gov.uk/def/school/> .");
+        builder.append("@prefix skos:    <http://www.w3.org/2004/02/skos/core#> .");
+        builder.append("@prefix ext:     <http://wwww.devexe.co.uk/vocabularies/ext#> .");
+        builder.append("@prefix dri:     <http://wwww.devexe.co.uk/vocabularies/dri#> .");
+        builder.append(":root a api:API");
+        builder.append("; api:sparqlEndpoint <here:dataPart>");
+        builder.append(inSpec);
+        builder.append("; api:endpoint :ep");
+        builder.append(".");
+        builder.append(":ep a api:ItemEndpoint");
+        builder.append("; api:variable [api:name \"uuid\"; api:type xsd:string]");
+        builder.append("; api:uriTemplate '/child-count/{uuid}'");
+        builder.append(inEndpoint);
+        builder.append(".");
+        builder.append("<here:dataPart> :elements <http://localhost:2020/dri/catalog/45b57b49-9abf-4fe0-b82d-87c984564a6c>,");
+        builder.append("<http://localhost:2020/dri/catalog/e2ddcb69-55b0-4816-9792-d0abab5dc6f9>,");
+        builder.append("<http://localhost:2020/dri/catalog/6a1eb17a-7c08-4927-a71a-0cd6c29c252a>");
+        builder.append(".");
+        builder.append("<http://localhost:2020/dri/catalog/45b57b49-9abf-4fe0-b82d-87c984564a6c> a dri:catalog ;");
+        builder.append("dri:catalogBatchUUID <http://localhost:2020/dri/batch/08dacdd9-9f81-4411-a87b-0b68185ecfa7> ;");
+        builder.append("dri:catalogCollectionUUID <http://localhost:2020/dri/collection/e9f3c8e9-e883-4fcf-a9a3-5caf0c808c5d> ;");
+        builder.append("dri:catalogKnown  \"2012-06-04T00:00:00\"^^xsd:dateTime ;");
+        builder.append("dri:catalogReference \"WO/16/409/27_31\"^^xsd:string ;");
+        builder.append("dri:catalogUUID  \"45b57b49-9abf-4fe0-b82d-87c984564a6c\"^^xsd:string .");
+        builder.append("<http://localhost:2020/dri/catalog/e2ddcb69-55b0-4816-9792-d0abab5dc6f9> a dri:catalog ;");
+        builder.append("dri:catalogBatchUUID <http://localhost:2020/dri/batch/08dacdd9-9f81-4411-a87b-0b68185ecfa7> ;");
+        builder.append("dri:catalogCollectionUUID <http://localhost:2020/dri/collection/e9f3c8e9-e883-4fcf-a9a3-5caf0c808c5d> ;");
+        builder.append("dri:catalogKnown  \"2012-06-04T00:00:00\"^^xsd:dateTime ;");
+        builder.append("dri:catalogParentUUID <http://localhost:2020/dri/catalog/45b57b49-9abf-4fe0-b82d-87c984564a6c> ;");
+        builder.append("dri:catalogReference \"WO/16/409/27_31/112\"^^xsd:string ;");
+        builder.append("dri:catalogUUID  \"e2ddcb69-55b0-4816-9792-d0abab5dc6f9\"^^xsd:string .");
+        builder.append("<http://localhost:2020/dri/catalog/6a1eb17a-7c08-4927-a71a-0cd6c29c252a> a dri:catalog ;");
+        builder.append("dri:catalogBatchUUID <http://localhost:2020/dri/batch/08dacdd9-9f81-4411-a87b-0b68185ecfa7> ;");
+        builder.append("dri:catalogCollectionUUID <http://localhost:2020/dri/collection/e9f3c8e9-e883-4fcf-a9a3-5caf0c808c5d> ;");
+        builder.append("dri:catalogKnown  \"2012-06-04T00:00:00\"^^xsd:dateTime ;");
+        builder.append("dri:catalogParentUUID <http://localhost:2020/dri/catalog/45b57b49-9abf-4fe0-b82d-87c984564a6c> ;");
+        builder.append("dri:catalogReference \"WO/16/409/27_31/142\"^^xsd:string ;");
+        builder.append("dri:catalogUUID  \"6a1eb17a-7c08-4927-a71a-0cd6c29c252a\"^^xsd:string .");
+
+		Model specModel = ModelIOUtils.modelFromTurtle(builder.toString());
 		Resource root = specModel.createResource( specModel.expandPrefix( ":root" ) );
 		
 		APISpec spec = SpecUtil.specFrom( root );
