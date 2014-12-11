90b48b56102e82b398fb75ed6dd0965d5d498ca5
rwalpole <robkwalpole@gmail.com>
2014-12-11 15:12:52 +0000

    [ignore] clean up imports

--- src/main/java/com/epimorphics/lda/restlets/RouterRestletSupportExt.java
+++ src/main/java/com/epimorphics/lda/restlets/RouterRestletSupportExt.java
@@ -1,12 +1,9 @@
 package com.epimorphics.lda.restlets;
 
-import com.epimorphics.lda.Version;
-import com.epimorphics.lda.cache.Cache;
 import com.epimorphics.lda.core.APIFactory;
 import com.epimorphics.lda.core.ModelLoader;
 import com.epimorphics.lda.exceptions.APISecurityException;
 import com.epimorphics.lda.routing.*;
-import com.epimorphics.lda.sources.AuthMap;
 import com.epimorphics.lda.specmanager.SpecManagerExtImpl;
 import com.epimorphics.lda.specmanager.SpecManagerFactory;
 import com.epimorphics.lda.specmanager.SpecManagerImpl;
@@ -14,21 +11,13 @@ import com.epimorphics.lda.specs.APISpec;
 import com.epimorphics.lda.specs.APISpecExt;
 import com.epimorphics.lda.support.EldaFileManager;
 import com.epimorphics.lda.vocabularies.API;
-import com.epimorphics.util.DOMUtils;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.shared.WrappedException;
-import com.hp.hpl.jena.util.FileManager;
-import com.hp.hpl.jena.util.Locator;
-import com.hp.hpl.jena.util.LocatorFile;
 import com.hp.hpl.jena.vocabulary.RDF;
-import org.apache.log4j.PropertyConfigurator;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -42,10 +31,6 @@ public class RouterRestletSupportExt extends RouterRestletSupport {
         //
         Router router = new DefaultRouter();
         String baseFilePath = ServletUtils.withTrailingSlash(con.getRealPath("/"));
-<<<<<<< HEAD
-=======
-        //AuthMap am = AuthMap.loadAuthMap( EldaFileManager.get(), noNamesAndValues );
->>>>>>> 2d9ca82cee965f0f07c1d970a53c3b0ea4627520
         ModelLoader modelLoader = new APIModelLoader( baseFilePath );
         addBaseFilepath( baseFilePath );
         //
@@ -53,11 +38,7 @@ public class RouterRestletSupportExt extends RouterRestletSupport {
         SpecManagerFactory.set(sm);
         //
         for (PrefixAndFilename pf: pfs) {
-<<<<<<< HEAD
-            RouterRestletSupportExt.loadOneConfigFile( router, "", modelLoader, pf.prefixPath, pf.fileName );
-=======
             loadOneConfigFile( router, "", modelLoader, pf.prefixPath, pf.fileName );
->>>>>>> 2d9ca82cee965f0f07c1d970a53c3b0ea4627520
         }
         int count = router.countTemplates();
         return count == 0  ? RouterFactory.getDefaultRouter() : router;
